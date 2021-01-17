22f26af2aa9228f0810d03fcb5d8f75d0a68fb21
YuanSamuel <samuel6161@gmail.com>
2021-01-17 02:30:51 -0600

    add manual entries, bugfixes

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import firebase from "firebase/app";
 function App() {
   const [user, setUser] = useState();
   const [dailyEntries, setDailyEntries] = useState();
+  const [generalUpdates, setGeneralUpdates] = useState();
 
   useEffect(() => {
     const userId = new URLSearchParams(window.location.search).get("user");
-    firestore
+    if (userId != null) {
+      firestore
       .collection("users")
       .doc(userId)
       .get()
@@ -24,14 +26,25 @@ function App() {
               user.data()["dailyEntries"]
             )
             .get()
-            .then((entries) => {
-              console.log(entries);
-              setDailyEntries(entries.docs);
+            .then((dailyEntries) => {
+              setDailyEntries(dailyEntries.docs);
+              firestore
+                .collection("manualEntries")
+                .where(
+                  firebase.firestore.FieldPath.documentId(),
+                  "in",
+                  user.data()["manualEntries"]
+                )
+                .get()
+                .then((manualEntries) => {
+                  setGeneralUpdates(manualEntries.docs);
+                });
             });
         } else {
           console.log("error");
         }
       });
+    }
   }, []);
 
   return user != null ? (
@@ -50,9 +63,21 @@ function App() {
           <h4>Weight: {user["weight"]} kg</h4>
         </div>
       </div>
-      <h2>Log</h2>
-      <div className="Log">
-        {dailyEntries != null ? <LogList docs={dailyEntries} /> : <div></div>}
+      <h2>Symptoms Log</h2>
+      <div className="Symptoms-log">
+        {dailyEntries != null ? (
+          <SymptomsLogList docs={dailyEntries} />
+        ) : (
+          <div></div>
+        )}
+      </div>
+      <h2>General Updates</h2>
+      <div className="General-updates">
+        {generalUpdates != null ? (
+          <GeneralUpdatesList docs={generalUpdates} />
+        ) : (
+          <div></div>
+        )}
       </div>
     </div>
   ) : (
@@ -60,7 +85,7 @@ function App() {
   );
 }
 
-function LogList(props) {
+function SymptomsLogList(props) {
   const docs = props.docs;
 
   docs.sort(
@@ -69,22 +94,72 @@ function LogList(props) {
   );
 
   return docs.map((doc) => (
-    <div className="DailyEntry" key={doc.id}>
+    <div className="Daily-entry" key={doc.id}>
       <p>Date: {doc.data()["date"].toDate().toDateString()}</p>
-      <p>Mood: {doc.data()["mood"] ?? 'N/A'}</p>
-      <p>Sleep: {doc.data()["sleep"] ?? 'N/A'} hours</p>
-      <p>Water: {doc.data()["calories"] ?? 'N/A'} cups</p>
-      <p>Calories Burned: {doc.data()["calories"] ?? 'N/A'} cal</p>
-      <p>Steps Taken: {doc.data()["steps"] ?? 'N/A'} steps</p>
-      <p>Exercise: {doc.data()["exercise"] ?? 'N/A'} minutes</p>
-      <p>Fatigue: {doc.data()["fatigue"] != null ? doc.data()["fatigue"] ? 'Yes' : 'No' : 'N/A'}</p>
-      <p>Fever: {doc.data()["fatigue"] != null ? doc.data()["fever"] ? 'Yes' : 'No' : 'N/A'}</p>
-      <p>Cough: {doc.data()["fatigue"] != null ? doc.data()["cough"] ? 'Yes' : 'No' : 'N/A'}</p>
-      <p>Congestion: {doc.data()["fatigue"] != null ? doc.data()["congestion"] ? 'Yes' : 'No' : 'N/A'}</p>
-      <p>Sore Throat: {doc.data()["fatigue"] != null ? doc.data()["sore throat"] ? 'Yes' : 'No' : 'N/A'}</p>
-      <p>Other Ailments: {doc.data()["otherAilments"] ?? 'N/A'}</p>
+      <p>Mood: {doc.data()["mood"] ?? "N/A"}</p>
+      <p>Sleep: {doc.data()["sleep"] ?? "N/A"} hours</p>
+      <p>Water: {doc.data()["calories"] ?? "N/A"} cups</p>
+      <p>Calories Burned: {doc.data()["calories"] ?? "N/A"} cal</p>
+      <p>Steps Taken: {doc.data()["steps"] ?? "N/A"} steps</p>
+      <p>Exercise: {doc.data()["exercise"] ?? "N/A"} minutes</p>
+      <p>
+        Fatigue:{" "}
+        {doc.data()["fatigue"] != null
+          ? doc.data()["fatigue"]
+            ? "Yes"
+            : "No"
+          : "N/A"}
+      </p>
+      <p>
+        Fever:{" "}
+        {doc.data()["fatigue"] != null
+          ? doc.data()["fever"]
+            ? "Yes"
+            : "No"
+          : "N/A"}
+      </p>
+      <p>
+        Cough:{" "}
+        {doc.data()["fatigue"] != null
+          ? doc.data()["cough"]
+            ? "Yes"
+            : "No"
+          : "N/A"}
+      </p>
+      <p>
+        Congestion:{" "}
+        {doc.data()["fatigue"] != null
+          ? doc.data()["congestion"]
+            ? "Yes"
+            : "No"
+          : "N/A"}
+      </p>
+      <p>
+        Sore Throat:{" "}
+        {doc.data()["fatigue"] != null
+          ? doc.data()["sore throat"]
+            ? "Yes"
+            : "No"
+          : "N/A"}
+      </p>
+      <p>Other Ailments: {doc.data()["otherAilments"] ?? "N/A"}</p>
     </div>
   ));
+}
+
+function GeneralUpdatesList(props) {
+  const docs = props.docs;
+
+  docs.sort(
+    (generalUpdateA, generalUpdateB) =>
+      generalUpdateB.data()["date"] - generalUpdateB.data()["date"]
+  );
+
+  return docs.map((doc) => <div className="General-update">
+    <p>Date: {doc.data()["date"].toDate().toDateString()}</p>
+    <p>Height: {doc.data()["height"] ?? "N/A"} cm</p>
+    <p>Weight: {doc.data()["weight"] ?? "N/A"} kg</p>
+  </div>)
 }
 
 export default App;
